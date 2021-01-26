1c040e6e4456b1c2bb7e547b7a2d6887cddfc9bc
frnzska <minziska@googlemail.com>
2021-01-26 20:57:27 +0100

    move serializer config to correct folder, remove workaround

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,10 @@
 class ArticlesController < ApplicationController
 
     def index
-        render json: Article.all, adapter: :json_api # serializer config not working, workaround for now
+        render json: Article.all
     end
 
     def show
-        render json: Article.find(params[:id]), adapter: :json_api 
+        render json: Article.find(params[:id])
     end
 end
