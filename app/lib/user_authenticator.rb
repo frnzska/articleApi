5bb8d7cfafafe66ac9cc062fa2078584a7ab782a
frnzska <minziska@googlemail.com>
2021-01-28 13:15:38 +0100

    clean up

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -9,28 +9,41 @@ class UserAuthenticator
     end
   
     def perform
-        
-        client = Octokit::Client.new(
-            client_id: ENV['GITHUB_CLIENT_ID'],
-            client_secret: ENV['GITHUB_CLIENT_SECRET']
-          )
-          token = client.exchange_code_for_token(code)
-          if token.try(:error).present?
+        if token.try(:error).present?
             raise AuthenticationError
-          else
-            user_client = Octokit::Client.new(
-                access_token: token)
-              user_data = user_client.user.to_h.
-                slice(:login, :avatar_url, :url, :name)
-            if User.exists?(login: user_data[:login]) 
-                @user = User.find_by(login: user_data[:login]) 
-            else
-              User.create(user_data.merge(provider: 'github'))
-            end
-          end
+        else
+            prepare_user
+        end
     end
 
     private
     attr_reader :code
+
+    def prepare_user
+        if User.exists?(login: user_data[:login]) 
+            user = User.find_by(login: user_data[:login]) 
+        else
+          User.create(user_data.merge(provider: 'github'))
+        end
+    end
+
+
+    def client 
+        @client ||= Octokit::Client.new(
+            client_id: ENV['GITHUB_CLIENT_ID'],
+            client_secret: ENV['GITHUB_CLIENT_SECRET']
+          )
+        end
+
+    def token
+        token = client.exchange_code_for_token(code)
+    end
+
+    def user_data
+        user_client = Octokit::Client.new(
+            access_token: token)
+          user_data = user_client.user.to_h.
+            slice(:login, :avatar_url, :url, :name)
+    end
 
 end
